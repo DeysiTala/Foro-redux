cb2677d7eef6975b64814621d87b603e8b5730aa
carlinha talavera cardenas <sude.90@gmail.com>
2017-10-25 23:28:18 -0500

    text tarea sin funcionamiento

--- foro-redux/src/App.js
+++ foro-redux/src/App.js
@@ -5,26 +5,40 @@ import { addTodo } from './actions';
 import { connect } from 'redux-zero/react';
 import './App.css';
 
-const App = ({ todos, posts }) => {
- 
+const App = ({ todos }) => {
+  const todoList = todos.map(todo => <li key={todo.id}> {todo.text} </li>);
+  const onSubmit = e => {
+     e.preventDefault();
+     addTodo(this.refInput.value);
+  };
    
    return (
       <div clasName ="container ">
         <div className ="row" >
           <div className ="col-xs-12 formulario">
-         <form>
+         <form onSubmit={onSubmit}>
          <div className="form-group">
            <label > </label>
-           <input type="text" className="form-control" placeholder="Escribe tu nombre" />
+           <input 
+           type="text"
+           name="name" 
+           className="form-control" 
+           placeholder="Escribe tu nombre"
+                       
+          ref={e => (this.refInput = e)} />
          </div>
          <div className="form-group">
-           <label>Comentario:</label>
-           <textarea className="form-control textarea" value="Escribe tu comentario"> </textarea>
+           <label>Comentario :</label>
+           <textarea className="form-control textarea" placeholder="Escribe tu comentario"> </textarea>
          </div>
          <button type="submit" className="btn btn-primary">Postear</button>
        </form>
        </div>
        </div>
+       <div className="main">
+            
+            <ul id="invitedList">{todoList}</ul>
+         </div>
        </div>
       
    );
